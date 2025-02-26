b02569f7842f304398915fac3c5b1d4a8becc026
Delia Paternina <delia.paternina@gmail.com>
2025-02-26 16:59:48 +0000

    adds construct to discountContext file

--- app/Services/DiscountContext.php
+++ app/Services/DiscountContext.php
@@ -8,6 +8,11 @@ class DiscountContext
 {
     private DiscountStrategy $strategy;
 
+    public function __construct(DiscountStrategy $strategy)
+    {
+        $this->strategy = $strategy;
+    }
+
     public function setStrategy(DiscountStrategy $strategy)
     {
         $this->strategy = $strategy;
